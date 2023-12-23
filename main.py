903da35b10b4f3c6ff650a8551d44b294022dcce
Vladimir Astapenko <wccvlast@mail.ru>
2023-12-23 07:44:55 +0300

    added def edit_contact in main.py file

--- main.py
+++ main.py
@@ -54,17 +54,38 @@ def delete_contact():
             easygui.msgbox(f"Запись {name} не найдена в телефонном справочнике.")
 
 
-# Тело программы
+# Функция для редактирования записи в телефонном справочнике
+def edit_contact():
+    name = easygui.enterbox("Введите имя контакта: ").lower()
+    with open('contacts.csv', 'r', encoding="windows-1251") as book:
+        reader = csv.reader(book)
+        rows = list(reader)
+    found = False
+    for row in rows:
+        if row[0] == name:
+            phone = easygui.enterbox(f"Введите новый номер телефона для {name}:")
+            row[1] = phone
+            found = True
+            break
+    if found:
+        with open('contacts.csv', 'w', newline='') as book:
+            writer = csv.writer(book)
+            writer.writerows(rows)
+        easygui.msgbox(f"Запись {name}: {phone} изменена в телефонном справочнике.")
+    else:
+        easygui.msgbox(f"Запись {name} не найдена в телефонном справочнике.")
 
+
+# Тело программы
 while True:
+# Графический интерфейс программы со списком действий
     choice = easygui.buttonbox("Выберите действие с телефонной книгой:",
                                choices=["Найти контакт",
                                         "Добавить новый контакт",
                                         "Удалить имеющуюся контакт",
                                         "Редактировать контакт",
                                         "Выход"])
-
-    # Выполняем действия
+# Выполнение действий в зависимости от выбранной комманды
     if choice == "Добавить новый контакт":
         create_contact()
     elif choice == "Удалить имеющуюся контакт":
